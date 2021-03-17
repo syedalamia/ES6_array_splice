b6c822d37f2196b0824f91dba4ab18d142d63b5f
lamianoorjahan <62371017+lamianoorjahan@users.noreply.github.com>
2021-03-17 18:05:09 +0600

    first commit

---
+++ array_splice.js
@@ -0,0 +1,7 @@
+var arr=[1,2,3,4,5];
+
+var result=arr.splice(1,3,10,11,15);
+
+console.log("here is the updated array\n",arr);//it'll return the updated array
+
+console.log(result)//number i've removed from array
